7cc73e1f15472ff02379b3fd9536e3e83c7981f8
Kushikime <demcsakerik@gmail.com>
2023-07-16 15:58:34 +0200

    02: simple selection sort algorithm

---
+++ selection-sort/selection-sort.js
@@ -0,0 +1,30 @@
+function findSmallestNumber(arr) {
+  smallest = arr[0];
+  smallestIndex = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallestIndex = i;
+    }
+  }
+
+  return smallestIndex;
+}
+
+function sortArray(arr) {
+  let sorted = [];
+
+  while (arr.length) {
+    let smallestNumIndex = findSmallestNumber(arr);
+    sorted.push(...arr.splice(smallestNumIndex, 1));
+  }
+
+  return sorted;
+}
+
+let arrTest = [233, 213, 321124, 4, 5, 43234, 432, 1];
+
+let sortedList = sortArray(arrTest);
+
+console.log("result: ", sortedList);
